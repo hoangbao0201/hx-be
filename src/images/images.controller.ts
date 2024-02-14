bd501b9fa17e47c10fa16ef3f00e6dab8bb2f53a
hoangbao0201 <hoangbao020103@gmail.com>
2024-02-14 08:54:50 +0700

    Fix Server Deploy Adaptable

--- src/images/images.controller.ts
+++ src/images/images.controller.ts
@@ -4,30 +4,30 @@ import axios from 'axios';
 import userAgent from 'random-useragent';
 
 
-@Controller('api/image')
+@Controller('api/images')
 export class ImagesController {
   constructor(private readonly imagesService: ImagesService) {}
 
-  @Get()
-  async findOne(@Query('slug') slug: string, @Res() res: Response) {
+  // @Get()
+  // async findOne(@Query('slug') slug: string, @Res() res: Response) {
 
-    const response = await axios.get(slug, {
-      responseType: 'stream',
-      // responseType: 'arraybuffer',
-      headers: {
-        referer: "https://lxmanga.net",
-        'Sec-Ch-Ua': '"Not A(Brand";v="99", "Google Chrome";v="121", "Chromium";v="121"',
-        'Sec-Ch-Ua-Mobile': "?0",
-        'Sec-Ch-Ua-Platform': "Windows",
-        'User-Agent': userAgent?.getRandom()
-      },
-    });
+  //   const response = await axios.get(slug, {
+  //     responseType: 'stream',
+  //     // responseType: 'arraybuffer',
+  //     headers: {
+  //       referer: "https://lxmanga.net",
+  //       'Sec-Ch-Ua': '"Not A(Brand";v="99", "Google Chrome";v="121", "Chromium";v="121"',
+  //       'Sec-Ch-Ua-Mobile': "?0",
+  //       'Sec-Ch-Ua-Platform': "Windows",
+  //       'User-Agent': userAgent?.getRandom()
+  //     },
+  //   });
 
     // return {
     //   success: true,
     //   image: JSON.stringify(response.data)
     // };
-    response.data.pipe(res);
-  }
+  //   response.data.pipe(res);
+  // }
   
 }
