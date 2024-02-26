a0630107a4b1387f4ade9976a6920f25bf621fd0
hoangbao0201 <hoangbao020103@gmail.com>
2024-02-26 20:54:34 +0700

    Fix Service Image

--- src/images/images.controller.ts
+++ src/images/images.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, Post, Body, Patch, Param, Delete, Query, Res } from '@
 import { ImagesService } from './images.service';
 import axios from 'axios';
 import userAgent from 'random-useragent';
+import { Response } from 'express';
 
 
 @Controller('api/images')
